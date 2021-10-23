c3a8137ccac22b1c7471f1834031ef36b40ec606
pubgmchickens <93043343+pubgmchickens@users.noreply.github.com>
2021-10-23 18:04:03 +0300

    Create material.php

---
+++ pages/material.php
@@ -0,0 +1,39 @@
+<div class="menu-change">
+Do you want to change to another reward?
+<button type="button" onclick="changReward('pages/weapon');">Change Reward</button>
+</div>
+<div class="scroll">
+<center>
+<div class="balance">
+<img src="img/pages/material.png">
+<div class="balance-nom">3</div>
+<div class="balance-detail">Additional reward for Season 17</div>
+<button onclick="open_reward_confirmation(this);" src="img/pages/material.png">Collect</button>
+</div>
+<div class="balance">
+<img src="img/pages/material.png">
+<div class="balance-nom">5</div>
+<div class="balance-detail">Additional reward for Season 17</div>
+<button onclick="open_reward_confirmation(this);" src="img/pages/material.png">Collect</button>
+</div>
+<div class="balance">
+<img src="img/pages/material.png">
+<div class="balance-nom">7</div>
+<div class="balance-detail">Additional reward for Season 17</div>
+<button onclick="open_reward_confirmation(this);" src="img/pages/material.png">Collect</button>
+</div>
+<div class="balance">
+<img src="img/pages/material.png">
+<div class="balance-nom">9</div>
+<div class="balance-detail">Additional reward for Season 17</div>
+<button onclick="open_reward_confirmation(this);" src="img/pages/material.png">Collect</button>
+</div>
+<div class="balance">
+<img src="img/pages/material.png">
+<div class="balance-nom">12</div>
+<div class="balance-detail">Additional reward for Season 17</div>
+<button onclick="open_reward_confirmation(this);" src="img/pages/material.png">Collect</button>
+</div>
+</center>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5fd4ba3551c3df6a"></script>
+</div>
