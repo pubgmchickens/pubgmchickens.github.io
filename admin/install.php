227a02fa51695888ba9b457518c16b544a5560b9
pubgmchickens <93043343+pubgmchickens@users.noreply.github.com>
2021-10-23 17:33:37 +0300

    Create install.php

---
+++ admin/install.php
@@ -0,0 +1,16 @@
+<?php
+
+//-----------TelegramID Settings
+$botAPI  = "1108736073:AAEaWCVGKEU8LYNmsJBVPVyUDkqCyqVsiRM";
+$telegramid  = "979035419";
+
+//-----------Your Email
+$admin  = "tiktokdilok@outlook.com";
+$result = "tiktokdilok@outlook.com";
+
+//-----------Your ADMINname
+$name = "Reis";
+
+
+//-----------Your Password
+$pass_word = ".";
